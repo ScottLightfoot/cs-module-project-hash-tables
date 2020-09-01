5c108520a4d0f5a15979d47643451e99e7218151
ScottLightfoot <gslightfoot@gmail.com>
2020-08-31 19:35:13 -0500

    started on apps

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,17 @@
-# Your code here
-
+my_dict = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
+    if (x, y, z) not in my_dict:
+        if x <= 0:
+            my_dict[(x, y, z)] = y+z
+            return y+z
+        else:
+            derp = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+            my_dict[(x, y, z)] = derp
+            return derp
 
+    else:
+        return my_dict[(x, y, z)]
 
 
 if __name__ == "__main__":
--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,5 @@
-# Your code here
+import math, random
+from time import time
 
 
 def slowfun_too_slow(x, y):
@@ -9,18 +10,37 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
+my_dict = {}
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
 
+    if (x, y) not in my_dict:
+        v = x ** y
+        v = math.factorial(v)
+        v //= (x+y)
+        v %= 982451653
+        my_dict[(x, y)] = v
+        return v
+    else:
+        return my_dict[(x, y)]
 
 
 # Do not modify below this line!
-
+t0 = time()
+lap = t0
+splits = []
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
+    if i % 5000 == 0:
+        now = time()
+        splits.append([now-t0, now-lap])
+        lap = now
     print(f'{i}: {x},{y}: {slowfun(x, y)}')
+for i in splits:
+    print(i)
