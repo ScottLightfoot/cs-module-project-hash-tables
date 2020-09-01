e24a82513dbf74a410df0b4d1f2b271586ada77a
ScottLightfoot <gslightfoot@gmail.com>
2020-09-01 17:12:16 -0500

    one to go

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,23 @@
-# Use frequency analysis to find the key to ciphertext.txt, and then
-# decode it.
+from collections import Counter
 
-# Your code here
+order_of_freq = ['E', 'T', 'A', 'O', 'H', 'N', 'R',
+                 'I', 'S', 'D', 'L', 'W', 'U', 'G',
+                 'F', 'B', 'M', 'Y', 'C', 'P', 'K',
+                 'V', 'Q', 'J', 'X', 'Z']
 
+with open("ciphertext.txt") as f:
+    words = f.read()
+
+letters = [i for i in words if "A" <= i <= "Z"]
+derp = Counter(letters)
+derpy = [(i, j) for i, j in derp.items()]
+derpy.sort(key = lambda x: x[1])
+derpy.reverse()
+
+cipher = {}
+for i in range(26):
+    cipher[derpy[i][0]] = order_of_freq[i]
+decoded = [cipher[i] if "A" <= i <= "Z" else i for i in words]
+out = ''.join(decoded)
+
+print(out)
--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,14 @@
-# Your code here
+import textwrap
+from collections import Counter
 
+with open("robin.txt") as f:
+    words = f.read()
+words = words.lower().split()
+derp = Counter(words)
+derpy = [(i, j) for i, j in derp.items() if j > 1]
+derpy.sort(key = lambda x: x[1])
+derpy.reverse()
+
+for i in derpy:
+    hist = f'{"#" * i[1]}'
+    print(f'{i[0]}: '.ljust(15), hist)
